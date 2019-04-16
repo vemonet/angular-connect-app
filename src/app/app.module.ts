ccc3b6662137a35648f974690a14f469d035e2fa
vemonet <vincent.emonet@gmail.com>
2019-04-17 00:45:58 +0200

    try to get profile info but not really working, also try to logout, not working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent, GoogleSigninComponent } from './app.component';
+import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent, GoogleSigninComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 
 @NgModule({
